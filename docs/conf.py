b948f04728895ea1867780d2e3a8a22dfd59ccb5
lvmgeo <lvmGEOGit@lvm.lv>
2017-06-12 10:53:52 +0300

    mock arcpy module in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -23,6 +23,31 @@ import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('..'))
 
+ON_RTD = os.environ.get('READTHEDOCS', None) == 'True'
+if ON_RTD:  # if we're on read-the-docs, then we need to mock out arcpy
+    class Mock(object):
+        def __init__(self, *args, **kwargs):
+            pass
+
+        def __call__(self, *args, **kwargs):
+            return Mock()
+
+        @classmethod
+        def __getattr__(cls, name):
+            if name in ('__file__', '__path__'):
+                return '/dev/null'
+            elif name[0] == name[0].upper():
+                mockType = type(name, (), {})
+                mockType.__module__ = __name__
+                return mockType
+            else:
+                return Mock()
+
+
+    MOCK_MODULES = ['arcpy']
+    for mod_name in MOCK_MODULES:
+        sys.modules[mod_name] = Mock()
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
